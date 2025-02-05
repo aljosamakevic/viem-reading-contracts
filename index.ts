88d0b0619d0d4e71d34ece0eeca6f32e3413cb47
Aljosa Makevic <aljosa100@gmail.com>
2025-02-06 03:30:46 +0400

    setup

--- index.ts
+++ index.ts
@@ -1,36 +1,36 @@
-import { http, createPublicClient, defineChain } from 'viem'
+// import { http, createPublicClient, defineChain } from 'viem'
 import { contract } from './contracts/simpleMessage.ts'
 import contractInfoTemplate from './contractInfoTemplate';
 
-export const stavanger = defineChain({
-  id: 50591822,
-  name: 'Stavanger',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: { http: ['https://rpc.stavanger.gateway.fm'] },
-  },
-  blockExplorers: {
-    default: { name: 'Blockscout', url: 'https://explorer.stavanger.gateway.fm' },
-  },
-  
-})
+// export const stavanger = defineChain({
+//   id: 50591822,
+//   name: 'Stavanger',
+//   nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+//   rpcUrls: {
+//     default: { http: ['https://rpc.stavanger.gateway.fm'] },
+//   },
+//   blockExplorers: {
+//     default: { name: 'Blockscout', url: 'https://explorer.stavanger.gateway.fm' },
+//   },
+// })
 
-const client = createPublicClient({
-  chain: stavanger,
-  transport: http(),
+// const client = createPublicClient({
+//   chain: stavanger,
+//   transport: http(),
 
-})
+// })
 
-const [message, owner] = await Promise.all([
-  client.readContract({
-    ...contract,
-    functionName: 'message',
-  }),
-  client.readContract({
-    ...contract,
-    functionName: 'owner',
-  }),
-])
+const [message, owner] = ["Hello, World!", "0x1234567890123456789012345678901234567890"];
+// const [message, owner] = await Promise.all([
+//   client.readContract({
+//     ...contract,
+//     functionName: 'message',
+//   }),
+//   client.readContract({
+//     ...contract,
+//     functionName: 'owner',
+//   }),
+// ])
 
 const htmlContent = contractInfoTemplate(contract.address, message, owner);
 export default htmlContent;
